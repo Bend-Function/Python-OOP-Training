64d983d5b651cbd43fc6af1ae9f162c40785e663
function@surface <bendfunction@outlook.com>
2020-10-27 17:55:55 +0800

    add 8-1

---
+++ src/Lab8/8-1.py
@@ -0,0 +1,16 @@
+class Person:
+    name = ""
+    age = 0
+    
+sam = Person()
+sam.name = "Sam"
+sam.age = 20
+
+james = Person()
+james.name = "James"
+james.age = 21
+
+print("Sam is %d" % sam.age)
+print("James is %d" % james.age)
+
+        
