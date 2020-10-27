d3fa09322a2bfd1c4146a924eace26f6a531b714
function@surface <bendfunction@outlook.com>
2020-10-27 17:43:19 +0800

    add 1-1

---
+++ src/Lab1/1-1.py
@@ -0,0 +1,8 @@
+# Author: Bend-Function
+
+def happyBirthday():
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday, dear ?")
+    print("Happy Birthday to you!")
+happyBirthday()
